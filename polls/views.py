d7210f125dc86f3f00a1680b5be4b95c46bf4baa
chaitanya0802 <chaitanya7447@gmail.com>
2024-05-31 10:55:33 +0530

    change in IndexView

--- polls/views.py
+++ polls/views.py
@@ -13,7 +13,6 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by("-pub_date")[:5]
-        print("dds")
 
 class DetailView(generic.DetailView):
     model = Question
